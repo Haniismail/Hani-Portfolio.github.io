fd655928d2c6efa558926d991e81cfd294a18563
Hani Ismail <hani@hani-badger.local>
2025-09-08 19:11:37 +0100

    updated cv

--- src/components/layouts/home/Banner.jsx
+++ src/components/layouts/home/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import HaniCV from '../../../../public/resume.pdf';
+import HaniCV from '../home/documents/resume.pdf';
 class Banner extends Component {
   render() {
     return (
